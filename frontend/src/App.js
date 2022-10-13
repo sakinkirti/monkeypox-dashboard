a65513ba8a1e7ec06e8fec7a50f824f429c3f730
Felix Huang <felixhuang12@gmail.com>
2022-10-13 00:58:10 -0400

    integrated leaflet with basic options

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 
 const Home = () => {
   return (
-    <div>
-      Testing
-    </div>
+    <MapContainer style={{ height: "98vh", width: "100%" }} center={[39.8283, -98.5795]} zoom={5} minZoom={4} maxZoom={10} zoomSnap={0.5} zoomDelta={0.5} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
   )
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import 'leaflet/dist/leaflet.css'
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';// renaming BrowserRouter import as Router
 
