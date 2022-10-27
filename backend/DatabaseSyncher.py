a3c8b347237a7c369c28386c59ea7604d791b1e1
sdendi <sakethdendi@gmail.com>
2022-10-27 16:31:40 -0400

    slight edits to syncher

--- backend/DatabaseSyncher.py
+++ backend/DatabaseSyncher.py
@@ -9,6 +9,7 @@ if calls the DatabaseUpdater Class to update our local database based on Global.
 It also contains a backup method in case the cron timer fails to update the database when needed.
 """
 
+import string
 import schedule
 import time
 
@@ -16,18 +17,25 @@ from DatabaseUpdater import DatabaseUpdater as du
 
 class DatabaseSyncher:
 
-    def __init__(self):
-        pass
-    
-    def job(self):
+    synchTime = ""
+
+    def __init__(self) -> None:
+        self.synchTime
+
+    def job():
         print("I'm working...")
 
     def synchTimer(self):
 
-        schedule.every().day.at("10:30").do(self.job())
-        schedule.every().day.at("11:30").do(du.db_connect)
-        schedule.every().day.at("11:30").do(du.db_connect)
+        self.setSynchTime("10:30")
+
+        schedule.every().day.at(self.synchTime).do(self.job)
+        schedule.every().day.at(self.synchTime).do(du.db_connect)
+        schedule.every().day.at(self.synchTime).do(du.db_update)
 
         while 1:
             schedule.run_pending()
             time.sleep(1)
+
+    def setSynchTime(self):
+        self.synchTime = self
