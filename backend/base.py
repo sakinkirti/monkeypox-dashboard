f31fa6e50d44274acd78db54de598bcb161f24a5
Felix Huang <felixhuang12@gmail.com>
2022-11-20 22:32:34 -0500

    edit api endpoint to distinguish pull for all state cumulative cases vs us total

--- backend/base.py
+++ backend/base.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 
 @app.route('/api/cases')
-def db_retrieve_all_state_cases():
+def db_retrieve_all_cumulative_state_cases():
     """
     method to retrieve data for specific state in database
     """
@@ -29,7 +29,51 @@ def db_retrieve_all_state_cases():
                                 'date', confirmed_date,
                                 'num_cases', num_cases
                             )
-                        ),
+                        )
+                    )
+                FROM (SELECT *
+                    FROM case_counts
+                    WHERE state_name='{state}'
+                    ORDER BY confirmed_date) as sub_table
+                WHERE state_name='{state}'
+                GROUP BY state_name
+            """
+        )
+        result.append(cursor.fetchall()[0][0])
+    conn.close()
+    newResult = []
+    for index, state in enumerate(result):
+        temp = []
+        for index2, row in enumerate(state["cases"]):
+            if index2 == 0:
+                temp.append(
+                    {"date": row["date"], "num_cases": row["num_cases"]})
+            else:
+                temp.append(
+                    {"date": row["date"], "num_cases": row["num_cases"]+temp[index2-1]["num_cases"]})
+        newResult.append({"state": state["name"], "cases": temp})
+    return newResult
+
+@app.route('/api/cases/total')
+def db_retrieve_per_day_state_cases():
+    """
+    method to retrieve data for specific state in database
+    """
+
+    du = DU()
+    conn = du.db_connect()
+    cursor = conn.cursor()
+    result = []
+    states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa",
+              "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire",
+              "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota",
+              "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+    for state in states:
+        cursor.execute(
+            f"""
+                SELECT
+                    json_build_object(
+                        'name', state_name,
                         'cumulative_cases', sum(num_cases)
                     )
                 FROM case_counts
@@ -89,7 +133,7 @@ def db_retrieve_state_cases():
     return newResult
 
 
-@app.route('/api/cases/total')
+@app.route('/api/cases/USTotal')
 def db_retrieve_US_total_cases():
     """
     method to retrieve cumulative US cases in database
