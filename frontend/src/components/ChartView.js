5d1b777754f05a40539aaaac2b3408c89562f175
Felix Huang <felixhuang12@gmail.com>
2022-11-25 15:10:24 -0500

    clean up chart view comments

--- frontend/src/components/ChartView.js
+++ frontend/src/components/ChartView.js
@@ -3,22 +3,6 @@ import { LineChart, ResponsiveContainer, XAxis, YAxis, Tooltip, Line, Legend, La
 import { Container, Text, Icon, HStack, Tabs, TabList, Tab, Heading } from '@chakra-ui/react'
 import caseService from '../services/cases'
 
-// const sampleData = [
-//     { day: "Feb 19, 2022", cases: 0, PR: "0.4%", IR: "1.2%", CFR: "3.0%" },
-//     { day: "Feb 20, 2022", cases: 102, PR: "0.4%", IR: "1.2%", CFR: "3.0%" },
-//     { day: "Jun 21, 2022", cases: 121, PR: "0.1%", IR: "1.9%", CFR: "3.3%" },
-//     { day: "Oct 22, 2022", cases: 132, PR: "0.9%", IR: "2.1%", CFR: "3.9%" },
-//     { day: "Oct 23, 2022", cases: 132, PR: "0.8%", IR: "2.1%", CFR: "3.9%" },
-//     { day: "Oct 24, 2022", cases: 134, PR: "0.7%", IR: "2.1%", CFR: "3.9%" },
-//     { day: "Oct 25, 2022", cases: 135, PR: "0.6%", IR: "2.1%", CFR: "3.9%" },
-//     { day: "Oct 26, 2022", cases: 135, PR: "0.5%", IR: "2.1%", CFR: "3.9%" },
-//     { day: "Oct 27, 2022", cases: 137, PR: "0.4%", IR: "2.1%", CFR: "3.9%" },
-//     { day: "Oct 28, 2022", cases: 138, PR: "0.3%", IR: "2.1%", CFR: "3.9%" },
-//     { day: "Oct 29, 2022", cases: 140, PR: "0.2%", IR: "2.1%", CFR: "3.9%" },
-//     { day: "Oct 30, 2022", cases: 140, PR: "0.1%", IR: "2.1%", CFR: "3.9%" },
-//     { day: "Oct 31, 2022", cases: 140, PR: "0.2%", IR: "2.1%", CFR: "3.9%" },
-// ]
-
 const CustomTooltip = ({ active, payload, label }) => {
     if (active && payload && payload.length) {
         return (
@@ -28,10 +12,6 @@ const CustomTooltip = ({ active, payload, label }) => {
                     <LineIcon boxSize={4} color='#EBFCF9' />
                     <Text color='#0E6495'>{`Case count: ${payload[0].payload.num_cases}`}</Text>
                 </HStack>
-                {/* <Divider borderColor='black' my={2} />
-                <Text color='#399BC6'>{`Prevalence Rate: ${payload[0].payload.PR}`}</Text>
-                <Text color='#00AEEA'>{`Incidence Rate: ${payload[0].payload.IR}`}</Text>
-                <Text color='#57BFBA'>{`Case-fatality Ratio: ${payload[0].payload.CFR}`}</Text> */}
             </Container>
         )
     }
